05385ef5a638a14a728059b015d6dbd4f3975245
Navita4 <navita441992@gmail.com>
2017-07-14 10:14:58 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,14 @@
+var student ={
+    name:"Lucky",
+    age :20,
+    dob :"6/8/2000",
+    pob :"Banglore",
+};
+var nameofstud =student.name;
+console.log(nameofstud);
+student.age = 20;
+console.log(student.age);
+student.dob = "6/8/2000";
+console.log(student.dob);
+student.pob = "Banglore";
+console.log(student.pob);
